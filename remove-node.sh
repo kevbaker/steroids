f60d8b3e4fd736a8426233ea48a01450db150cc8
Kevin Baker <kbaker@kev.io>
2014-06-17 19:20:21 -0700

    updated ruby version. changed package.json to use git:// urls since https git combo was failing

--- remove-node.sh
+++ remove-node.sh
@@ -4,10 +4,15 @@ sudo npm cache clean
 sudo rm -rf /usr/local/lib/node
 sudo rm -rf /usr/local/lib/node_modules
 
-sudo rm /usr/local/bin/node
-sudo rm /usr/local/bin/npm
-sudo rm /usr/local/bin/node-gyp
-sudo rm /usr/local/bin/node-waf
+sudo rm -rf /usr/local/bin/node
+sudo rm -rf /usr/local/bin/npm
+sudo rm -rf /usr/local/bin/node-gyp
+sudo rm -rf /usr/local/bin/node-waf
+
+sudo rm -rf /usr/local/include/node
+sudo rm -rf /usr/local/include/node_modules
 
 sudo rm -rf $HOME/.npm
 sudo rm -rf $HOME/.node-gyp
+
+
